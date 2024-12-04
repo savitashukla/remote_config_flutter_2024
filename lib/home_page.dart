78302d95b2e6d6e5ab886328a0568196f57be27d
ks31734 <savita.shukla@kotak.com>
2024-12-04 23:40:20 +0530

    remove un use code

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,11 +1,8 @@
-// Copyright 2019 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+
 import 'dart:async';
 
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
